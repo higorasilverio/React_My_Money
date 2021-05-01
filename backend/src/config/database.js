f0690e0c54857952d5cb7e86157ec4bb7a9148c7
higorasilverio <higorasilverio@outlook.com>
2021-05-01 11:59:31 -0300

    Backend: routes do get summary and count implemented

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -5,3 +5,9 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+// One way to change the required error message
+mongoose.Error.messages.general.required = "The '{PATH}' attribute is mandatory!"
+mongoose.Error.messages.Number.min = "'{VALUE}' is lower than the minimum value acceptable: '{MIN}', for '{PATH}'!"
+mongoose.Error.messages.Number.max = "'{VALUE}' is higher than the maximum value acceptable: '{MAX}', for '{PATH}'!"
+mongoose.Error.messages.String.enum = "'{VALUE}' is not valid for the attribute '{PATH}'"
