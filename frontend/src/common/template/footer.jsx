03a0f33db8eeed12ea433c5fc92e1f4543243d57
higorasilverio <higorasilverio@outlook.com>
2021-05-02 09:29:03 -0300

    Frontend: project configured and main html layout set

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2021 &nbsp;
+            <a href="https://github.com/higorasilverio" target='_blank'>
+                higorasilverio <i className="fa fa-github"></i>
+            </a>
+        </strong>
+    </footer>
+)
