0a014a02e0051a7a891ed789fe5218fcfe0ce445
higorasilverio <higorasilverio@outlook.com>
2021-05-03 20:13:26 -0300

    Frontend: add redux to project

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1>Billing Cycle</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class BillingCycles extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Billing Cycles' small='Register' />
+                <Content>
+                    Billing Cycles
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycles
--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,23 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+    componentDidMount() {
+        this.props.getSummary()
+    }
     render() {
+        const { credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader title="Dashboard" small='Version 1.0' />
                 <Content>
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank' 
-                            value='U$ 10.00' text='Credits total'/>
+                            value={`U$ ${credit.toFixed(2)}`} text='Credits total'/>
                         <ValueBox cols='12 4' color='red' icon='credit-card' 
-                            value='U$ 10.00' text='Debts total'/>
+                            value={`U$ ${debt.toFixed(2)}`} text='Debts total'/>
                         <ValueBox cols='12 4' color='blue' icon='money' 
-                            value='U$ 0.00' text='Consolidated value'/>
+                            value={`U$ ${credit - debt.toFixed(2)}`} text='Consolidated value'/>
                     </Row>
                 </Content>
             </div>
@@ -25,4 +32,7 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = state =>({summary: state.dashboard.summary})
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
